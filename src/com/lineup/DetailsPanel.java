0e31c1903e70ecbfd04d72024b24ebab880f4bbf
JonMercer <Odin@Odins-MacBook-Pro.local>
2015-07-22 20:04:03 -0700

    added gridbag layout

--- src/com/lineup/DetailsPanel.java
+++ src/com/lineup/DetailsPanel.java
@@ -28,5 +28,53 @@ public class DetailsPanel extends JPanel {
 
         //where to want each class to go
         GridBagConstraints gc = new GridBagConstraints();
+
+        //Divide contolls into logical sections
+
+
+        /// First Collumn ////
+        //top left is 0,0
+
+        //how much of the scren will be taken
+
+        gc.anchor = GridBagConstraints.LINE_END;//allign to the right
+        gc.weightx = 0.5;
+        gc.weighty = 0.5;
+
+
+        gc.gridx = 0;
+        gc.gridy = 0;
+        add(nameLabel, gc);
+
+        gc.gridx = 0;
+        gc.gridy = 1;
+        add(occupationLabel, gc);
+
+
+        //second collumn
+
+        gc.anchor = GridBagConstraints.LINE_START;
+        gc.gridx = 1;
+        gc.gridy = 0;
+        add(nameField, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 1;
+        add(occupationField, gc);
+
+        //Final row
+
+        gc.weighty = 10;
+
+        gc.fill = GridBagConstraints.BOTH;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        gc.gridx = 1;
+        gc.gridy = 2;
+        add(addBtn, gc);
+
+
+
+
+
     }
 }
