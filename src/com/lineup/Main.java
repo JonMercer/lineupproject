b7399f044d718f035da62d6e5c401afd26af1eb4
Jon Mercer <mercer.jon@gmail.com>
2015-07-25 13:02:45 -0700

    created simple skeleton with button. Still need to work on gridbaglayout and object persistence

--- src/com/lineup/Main.java
+++ src/com/lineup/Main.java
@@ -11,18 +11,21 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class Main {
 
     private final String NAME_OF_APP = "LineUp";
-    private final int APPLICATION_WIDTH = 500;
-    private final int APPLICATION_HEIGHT = 500;
+    private final int WINDOW_WIDTH = 500;
+    private final int WINDOW_HEIGHT = 500;
 
     private Main() {
         System.out.println("hello world");
         generateFrame();
     }
 
-
+    /*
+    * The main entry method.
+    *
+    * */
     public static void main(String[] args) {
         Main m = new Main();
-        
+
     }
 
     private void generateFrame() {
@@ -31,13 +34,13 @@ public class Main {
             @Override
             public void run() {
                 //Main window of application. Only one per swing program
-                JFrame frame = new JFrame(NAME_OF_APP);
+                JFrame frame = new MainFrame(NAME_OF_APP);
 
                 //sets the frame to be visible
                 frame.setVisible(true);
                 //closes application properly
                 frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-                frame.setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
+                frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
             }
         });
     }
---
+++ src/com/lineup/MainFrame.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Our custom version of JFrame.
+ * We can add components here
+ * - layout manager: design layouts that reflow depending on screen state
+ * - swing components: base class for swing components (pieces of UI including keybinding)
+ * , and content pane
+ * Created by Jon Mercer on 15-07-23.
+ */
+public class MainFrame extends JFrame {
+    private static final String BUTTON_DOWN_LABEL = "down";
+    private static final String BUTTON_UP_LABEL = "up";
+    private static final String BUTTON_PRINT_LABEL = "print";
+    private static final String BUTTON_RESET_LABEL = "reset";
+
+
+//    https://docs.oracle.com/javase/tutorial/uiswing/layout/visual.html
+//    http://da2i.univ-lille1.fr/doc/tutorial-java/ui/features/components.html
+
+    public MainFrame(String title) {
+        super(title);
+
+        // Set layout manager. 5 panes, one in centre
+        setLayout(new BorderLayout());
+
+        //create swing component.
+        final JLabel numberLabel = new JLabel("50");
+        numberLabel.setSize(40, 40);
+
+        JButton downButton = new JButton(BUTTON_DOWN_LABEL);
+        JButton upButton = new JButton(BUTTON_UP_LABEL);
+        JButton printButton = new JButton(BUTTON_PRINT_LABEL);
+        JButton resetButton = new JButton(BUTTON_RESET_LABEL);
+
+        //add swing component to content pane.
+        Container container = getContentPane();
+
+        container.add(numberLabel, BorderLayout.CENTER);
+        container.add(downButton, BorderLayout.PAGE_END);
+        container.add(upButton, BorderLayout.PAGE_START);
+        container.add(printButton, BorderLayout.LINE_START);
+        container.add(resetButton, BorderLayout.LINE_END);
+
+
+        //annonymous class
+        downButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                numberLabel.setText("49");
+            }
+        });
+
+    }
+}
