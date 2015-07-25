9dbf6009e52807fa0f31c09d970e328fe46a2c39
JonMercer <mercer.jon@gmail.com>
2015-07-25 16:32:58 -0700

    created docs for controller class

--- src/com/lineup/controller/ControllerImpl.java
+++ src/com/lineup/controller/ControllerImpl.java
@@ -3,6 +3,8 @@ package controller;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * Controls all logic of button presses.
+ * 
  * Created by Jon Mercer on 15-07-25.
  */
 public class ControllerImpl implements Controller {
